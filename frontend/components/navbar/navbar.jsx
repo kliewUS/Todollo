7d0f3ca245b3b9c1b308526a386f1760c68849fb
Khai Yuan Liew <kyuliew@gmail.com>
2020-06-23 18:37:32 -0700

    Adjusted navbar styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -4,16 +4,21 @@ import { Link } from 'react-router-dom';
 export default ({ currentUser, logoutUser }) => { //{} Destructing
     const welcome = () => (
         <nav className="nav-bar">
-            <h3>Hello, {currentUser.username}!</h3>
+            <h3>{currentUser.username}</h3>
             <button onClick={logoutUser}>Log Out</button>
         </nav>        
     );
 
     const login = () => (
-        <nav className="nav-bar">
+      <nav className="nav-bar">
+        <nav className="nav-bar-left">
+            <h1 className="logo">Todollo</h1>
+        </nav>
+        <nav className="nav-bar-right">
             <Link className="nav-btn" to="/login">Log In</Link>
             <Link className="nav-btn" to="/signup">Sign Up</Link>
         </nav>        
+      </nav>  
     );
 
     return currentUser ? welcome() : login();
