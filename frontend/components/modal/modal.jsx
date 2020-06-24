cba27490b37a5e56d52ca4e99b306a607fdec124
Khai Yuan Liew <kyuliew@gmail.com>
2020-06-24 16:24:19 -0700

    Tweaked dropdown backgrounds

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -13,29 +13,34 @@ function Modal({modal, closeModal}) {
   }
 
   let component;
+  let background = 'transparent';
 
   switch (modal) {
     case 'account-menu':
         component = <AccountMenuContainer />;
+        background = 'none';
         break;      
     case 'notification-menu':
         component = <NotificationsMenu />;
+        background = 'none';        
         break;      
     case 'information-menu':
         component = <InformationMenu />;
+        background = 'none';        
         break;
     case 'board-create-menu':
         component = <BoardCreateMenu />;
         break;      
     case 'board-nav-menu':
         component = <BoardNavMenu />;
+        background = 'none';        
         break;      
     default:
       return null;
   }
   
   return (
-    <div className="modal-background" onClick={closeModal}>
+    <div className={`modal-background ${background}`} onClick={closeModal}>
       <div className="modal-child" onClick={e => e.stopPropagation()}>
         { component }
       </div>
--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -53,32 +53,3 @@ class NavBar extends React.Component{
 }
 
 export default NavBar;
-
-
-//May need to refactor this component.
-// export default ({ currentUser, logoutUser }) => { //{} Destructing
-//     const navBar = () => (
-//         <nav className="nav-bar">
-
-//             <nav className="nav-bar-left">
-//                 <button className="nav-btn-2">Home</button>
-//                 <button className="nav-btn-2">Boards</button>
-//                 <input className="search-bar" type="text"/>
-//             </nav>
-
-//             <nav className="nav-bar-center">
-//                 <h1 className="logo">Todollo</h1>            
-//             </nav>
-
-//             <nav className="nav-bar-right">
-//                 <button className="nav-btn-2">Create New Board</button>
-//                 <button className="nav-btn-2">Information</button>
-//                 <button className="nav-btn-2">Notifications</button>
-//                 <button className="logout-btn" onClick={logoutUser}>{currentUser.username.substring(0, 1)}</button>
-//             </nav>
-
-//         </nav>        
-//     );
-
-//     return navBar();
-// };
