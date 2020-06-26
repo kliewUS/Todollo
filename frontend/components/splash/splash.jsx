e857ed340c37f579abcfe9d495e9845d8b765333
Khai Yuan Liew <kyuliew@gmail.com>
2020-06-25 17:18:42 -0700

    Updated Splash styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,7 +6,18 @@ class Splash extends React.Component{
         return(
             <div>
                 <LoginBar />
-                {/* <h1>Welcome to Todollo!</h1> */}
+                <section id="splash">
+                    <div className="splash-text">
+                        <h1>Todollo lets you work together easily and achieve more.</h1>
+                        <p>Todollo organizes your projects in a flexible and rewarding way. Our boards, lists, cards can help you achieve more in your projects</p>
+                    </div>
+                </section>
+
+                <section id="info"></section>
+
+                <section id="demo"></section>
+
+                <section id="get-started"></section>
             </div>
         );
     }
