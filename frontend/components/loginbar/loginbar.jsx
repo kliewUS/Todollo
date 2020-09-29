53bb9bf7b2c38d357db029b72a969f555c6e6f82
kliewUS <kyuliew@gmail.com>
2020-09-28 17:43:15 -0700

    Updated Splash Page Styling

--- frontend/components/loginbar/loginbar.jsx
+++ frontend/components/loginbar/loginbar.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 
 const LoginBar = () => {
     return (
+    <div className="login-bar-container">
         <nav className="login-bar">
-        <nav className="login-bar-left">
-            <Link to='/' className="login-logo">Todollo</Link>
-        </nav>
-        <nav className="login-bar-right">
-            <Link className="login-btn" to="/login">Log In</Link>
-            <Link className="login-btn" to="/signup">Sign Up</Link>
-        </nav>        
-      </nav>  
+            <nav className="login-bar-left">
+                <Link to='/' className="login-logo">Todollo</Link>
+            </nav>
+            <nav className="login-bar-right">
+                <Link className="login-btn" to="/login">Log In</Link>
+                <Link className="login-btn" to="/signup">Sign Up</Link>
+            </nav>        
+        </nav>  
+    </div>
     );
 }
 
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginBar from '../loginbar/loginbar';
+import { Link } from 'react-router-dom';
 
 class Splash extends React.Component{
     render(){
         return(
             <div>
                 <LoginBar />
+
                 <section id="splash">
                     <div className="container">
                         <div className="splash-text">
@@ -17,6 +19,65 @@ class Splash extends React.Component{
                         </div>
                     </div>
                 </section>
+
+                <section id="teamwork-section">
+                    <div className="teamwork-container">
+
+                        <div>
+                            <div className="teamwork-text">
+                                <h2>Work with any team you like</h2>
+                                <p>Whether it's for a job, a hobby, or a family event. Todollo helps keep your team organized.</p>
+                                <Link className="splash-login-btn" to="/signup">Start working →</Link>
+                            </div>
+                        </div>
+
+                        <div className="teamwork-img">
+                            <img src="https://gist.githubusercontent.com/kliewUS/049d8cfda828ab0fe5a9caaf7270c6aa/raw/36896678a2d9eceddfe026434e8f9ea0df615fcd/8d86a350-28d8-4c1d-9a84-f5cc84d9ac33-resized.png" />
+                        </div>
+                    </div>
+                </section>
+
+                <section id="info-section">
+                    <div className="info-container">
+                        <div className="info-img">
+                            <img src="https://gist.githubusercontent.com/kliewUS/049d8cfda828ab0fe5a9caaf7270c6aa/raw/24dcf48c459a09ac70a3466f2e64a0bf4654ed2a/6919a1ab-3749-48eb-ab55-23c3b9ded246.png" />
+                        </div>
+                        <div>
+                            <div className="info-text">
+                                <h2>Information at a peek</h2>
+                                <p>See into the details by adding comments and labels directly to Todollo Cards. Work together on project from start to finish.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="startup-section">
+                    <div className="startup-container">
+                        <div className="startup-text">
+                            <h2>Start Planning Now</h2>
+                            <p>Sign up and be a part of the millions of teams who are using Todollo to get more done.</p>
+                            <Link className="splash-login-btn" to="/signup">Get Started - It's Free!</Link>
+                        </div>
+                    </div>
+                </section>
+
+
+                <footer id="splash-footer">
+                    <ul id="footer-menu">
+                        <li>
+                            <img src="https://gist.githubusercontent.com/kliewUS/049d8cfda828ab0fe5a9caaf7270c6aa/raw/24dcf48c459a09ac70a3466f2e64a0bf4654ed2a/icons8-github-24.png" />
+                            <a href="https://github.com/kliewUS">Github</a>
+                        </li>
+                        <li>
+                            <img src="https://gist.githubusercontent.com/kliewUS/049d8cfda828ab0fe5a9caaf7270c6aa/raw/24dcf48c459a09ac70a3466f2e64a0bf4654ed2a/icons8-linkedin-24.png" />
+                            <a href="#">Linkedin</a>
+                        </li>
+                        <li>
+                            <img src="https://gist.githubusercontent.com/kliewUS/049d8cfda828ab0fe5a9caaf7270c6aa/raw/24dcf48c459a09ac70a3466f2e64a0bf4654ed2a/icons8-angellist-24.png" />
+                            <a href="#">Angelist</a>
+                        </li>
+                    </ul>
+                </footer>
             </div>
         );
     }
